7c457d53d495a04c3aa9abd258d8f56286d884bd
Pranav5956 <pranavbalaji01@gmail.com>
2022-02-26 16:28:38 +0530

    initial commit

---
+++ Readme.md
@@ -0,0 +1,2 @@
+# Reinforcement Learning - Wordle
+This is a Reinforcement Learning project to make an agent play the game "Wordle".
---
+++ main.py
@@ -0,0 +1,27 @@
+import argparse
+from config import *
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(
+        description="Wordle Agent using Reinforcement Learning")
+    sub_parser = parser.add_subparsers(help="Choose run mode", dest="mode")
+
+    train = sub_parser.add_parser("train")
+    train.add_argument("-ep", type=int,
+                       default=EPISODES, help="Number of episodes")
+    train.add_argument("-lr", type=float,
+                       default=LEARNING_RATE, help="Learning Rate")
+    train.add_argument("-er", type=float,
+                       default=EXPLORATION_RATE, help="Exploration Rate")
+    train.add_argument("-df", type=float,
+                       default=DISCOUNT_FACTOR, help="Discount Factor")
+
+    sub_parser.add_parser("infer")
+    sub_parser.add_parser("demo")
+
+    args = parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
